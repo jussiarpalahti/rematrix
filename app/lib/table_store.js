64830f872264ce69471c73297e2429f51bb460f6
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-08-09 01:10:25 +0300

    Added table list loader to store

--- app/lib/table_store.js
+++ app/lib/table_store.js
@@ -1,3 +1,44 @@
-/**
- * Created by jussiarpalahti on 9.8.2015.
- */
+import lodash from 'lodash';
+var _ = lodash;
+
+import {
+    fetch_table_previews
+} from './table_utils'
+
+let TableStore = () => {
+
+    let obj = {
+        tables: {},
+        chosen: {},
+        matrix: {}
+    };
+
+    obj.get_table = get_table.bind(obj)
+    obj.get_fullmeta = get_fullmeta.bind(obj)
+    obj.get_fullmeta = get_fullmeta.bind(obj);
+    obj.get_fullmeta = get_fullmeta.bind(obj);
+    obj._populate_tablestore = _populate_tablestore.bind(obj);
+    obj._fetch_matrix = _fetch_matrix.bind(obj);
+
+};
+
+function get_table(tableid) {
+
+}
+
+function set_choices(tableid, choices) {
+
+}
+
+function _fetch_matrix(tableid) {
+
+}
+
+function _populate_tablestore(tableid) {
+    let cb = (data) => {
+        _.map(data.pxdocs, (table, index) => {
+            this.tables[table.name] = table;
+        });
+    };
+    fetch_table_previews(cb);
+}
