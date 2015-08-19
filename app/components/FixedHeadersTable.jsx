937d5dd02a5cb7f576d518487a934ab28cd0b373
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-08-19 20:16:33 +0300

    Moved heading dimension collection to FixedHeaderTable's top component
    Now it merely logs its findings for the full table, but there should
    be enough data to identify each cell correctly in other tables.

--- app/components/FixedHeadersTable.jsx
+++ app/components/FixedHeadersTable.jsx
@@ -19,12 +19,28 @@ export var FullTable = React.createClass({
 });
 
 export var FixedHeadersTable = React.createClass({
+
+    save_dimensions: function(node, heading, index, thindex, axis) {
+        let styles = window.getComputedStyle(node);
+        console.log({
+            axis: axis,
+            heading: heading.heading,
+            header: heading.header,
+            index: index,
+            thindex: thindex,
+            node: node,
+            cell_index: node.cellIndex,
+            height: styles.getPropertyValue('height'),
+            width: styles.getPropertyValue('width')
+        });
+    },
+
     render: function () {
         let table = this.props.table;
         return <div>
             <div id="heading" className="pos">
                 <table className="pure-table pure-table-bordered">
-                    <ColumnHeaders table={table} />
+                    <ColumnHeaders table={table} skip_data={true} />
                 </table></div>
             <div id="stub" className="pos">
                 <table className="pure-table pure-table-bordered">
@@ -32,8 +48,8 @@ export var FixedHeadersTable = React.createClass({
                 </table></div>
             <div id="cells" className="pos">
             <table className="pure-table pure-table-bordered">
-                <ColumnHeaders table={table} />
-                <DataCells table={table} />
+                <ColumnHeaders table={table} save_dimensions={this.save_dimensions} />
+                <DataCells table={table} save_dimensions={this.save_dimensions} />
             </table></div>
             </div>;
     }
@@ -52,19 +68,31 @@ export var ColumnHeaders = React.createClass({
                 table.heading_hopper(col_index),
                 (heading, thindex) => {
                     if (heading) {
-                        columns[thindex].push(<th key={col_index + '_' + thindex}
-                                                  colSpan={heading.hop}
-                                                  ref={
-                                                  function(component){
-                                        let node = React.findDOMNode(component);
-                                        (node!==null) && (node.style!==null) ? console.log(get_dimensions(node)) : '';}
+                        let col;
+                        if (this.props.save_dimensions) {
+                            col = <th key={col_index + '_' + thindex}
+                                colSpan={heading.hop}
+                                ref={
+                                (component) => {
+                                let node = React.findDOMNode(component);
+                                (node!==null) && (node.style!==null)
+                                ? this.props.save_dimensions(
+                                    node, heading, col_index, thindex, 'heading')
+                                : '';
+                                }
                             }>{heading.header}
-                            <img src="/app/stylesheets/empty.png" />
-                            </th>);
+                                <Dimension table={table} spaces={this.props.spaces} />
+                            </th>
+                        } else {
+                            col = <th key={col_index + '_' + thindex}
+                                      colSpan={heading.hop}>
+                                {heading.header}
+                                </th>
+                        }
+                        columns[thindex].push(col);
                     }
                 });
         }
-
         return <thead>{
             columns.map((heading, index) => {
             if (index==0) {
@@ -81,24 +109,35 @@ export var ColumnHeaders = React.createClass({
             }
             })
         }</thead>;
-
     }
 });
 
-let RowHeader = function (table, index) {
+let RowHeader = function (table, index, save_dimensions, spaces) {
     return _.map(
         table.stub_hopper(index),
         (heading, thindex) => {
             if (heading) {
-                return <th key={index + '_' + thindex}
-                           rowSpan={heading.hop}
-                            ref={function(component){
-                                let node = React.findDOMNode(component);
-                                (node!==null) && (node.style!==null) ? console.log(get_dimensions(node)) : '';}
-                            }>
-                    {heading.header}
-                    <img src="/app/stylesheets/empty.png" />
-                </th>;}
+                if (save_dimensions) {
+                    return <th
+                        key={index + '_' + thindex}
+                        rowSpan={heading.hop}
+                        ref={function(component){
+                        let node = React.findDOMNode(component);
+                        (node!==null) && (node.style!==null)
+                        ? save_dimensions(node, heading, index, thindex, 'stub')
+                        : '';
+                        }}>
+                        {heading.header}
+                        <Dimension table={table} spaces={spaces} />
+                    </th>;
+                } else {
+                    return <th
+                        key={index + '_' + thindex}
+                        rowSpan={heading.hop}>
+                        {heading.header}
+                        </th>;
+                }
+            }
             else return null;
         });
 };
@@ -108,31 +147,27 @@ export var DataCells = React.createClass({
         let table = this.props.table;
         return <tbody>{
             table.matrix.map((row, index) => {
-            return <tr className={(index % 2) != 0 ? "pure-table-odd" : ""} key={index}>{
-                [
-                    RowHeader(table, index),
-                    this.props.skip_data
-                    ? null
-                    : row.map((cell, cindex) => {
-                        return <td key={index + '_' + cindex}>
-                            {cell}
-                        </td>})
-                ]
+            return <tr
+                className={(index % 2) != 0 ? "pure-table-odd" : ""}
+                key={index}>
+                {
+                    [
+                        RowHeader(table, index, this.props.save_dimensions),
+                        this.props.skip_data
+                        ? null
+                        : row.map((cell, cindex) => {
+                            return <td key={index + '_' + cindex}>
+                                {cell}
+                            </td>})
+                    ]
             }</tr>
             })
         }</tbody>;
     }
 });
 
-let get_dimensions = (node) => {
-    let styles = window.getComputedStyle(node);
-    return {
-        node: node,
-        rows: node.rows,
-        cells: node.cells,
-        row_index: node.rowIndex,
-        cell_index: node.cellIndex,
-        height: styles.getPropertyValue('height'),
-        width: styles.getPropertyValue('width')
-    };
-};
+export var Dimension = React.createClass({
+    render: function () {
+        return <img src="/app/stylesheets/empty.png" />
+    }
+});
