23def329996854dabe6b716f54b1a724b33f4364
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-11-12 21:37:11 +0200

    Got pipeline to update view with React.render
    I don't know if building a root component just for app state or
    this approach where I rebuild root component and have React
    render it to an DOM node containing existing view. Supposedly this should not
    be slower.
    
    Since setProps is deprecated, it's either go with the root component state
     or re-render. Latter seems a little too forced, former needs awkward
    feeling wrapper component that uses props at first and then calls setState
    when updates happen.

--- app/lib/piper.js
+++ app/lib/piper.js
@@ -1,13 +1,29 @@
 
-import * as R from "ramda";
+import R from "ramda";
 
-export function get_data () {
+export function get_data (callback) {
+    let url = 'http://localhost:8000/';
     console.log("piper called");
     var f = R.pipeP(
-        () => fetch('http://localhost:8000/'),
+        () => fetch(url),
         (response) => response.json(),
-        (data) => console.log(data)
+        (data) => {
+            console.log("pipe response", data);
+            callback(data);
+        }
     );
     f();
 }
 
+export function piping (start, callback) {
+    console.log("pipe started");
+    var f = R.pipe(
+        (data) => data,
+        (data) => {
+            data.arg = data.arg + 1;
+            callback(data);
+        }
+    );
+    f(start, callback);
+    console.log("piped");
+}
--- app/remain.tsx
+++ app/remain.tsx
@@ -2,25 +2,51 @@
 
 //import React from 'react';
 import * as React from "react";
-import {get_data} from "./lib/piper.js";
+import {get_data, piping} from "./lib/piper.js";
 
 interface Props {
     foo: string;
+    arg: number;
+    piper: Function
 }
 
-export default class Tyyppi extends React.Component<Props, {}> {
+interface data {
+    any: any;
+}
+
+export class Tyyppi extends React.Component<Props, {}> {
+
+    handler() {
+        this.props.piper();
+    }
+
     render() {
-        return <span>Rendered with types: {this.props.foo}</span>
+        return <div>
+            <div><button onClick={this.handler.bind(this)}>Fetch data</button></div>
+            <div><span>Rendered with types: {this.props.foo}</span></div>
+            <div>Data: {this.props.arg}</div>
+        </div>
     }
 }
 
-declare var fetch: any;
+export class Main extends React.Component<any, {}> {
 
-function piper() {
-    console.log("pipe started");
-    get_data();
-    console.log("piped");
+    render() {
+        let data = this.props.data;
+        return <div>
+            <h1>React Typed Table Viewer</h1>
+            <div><Tyyppi arg={data.arg} foo={data.foo} piper={data.my_pipe} /></div>
+        </div>
+    }
 }
+
+const piper = function () {
+    console.log("pipe started");
+    get_data((data) => {
+        return null;
+    });
+    console.log("piped");
+};
 
 function main() {
     var app = document.getElementById('app');
@@ -30,11 +56,30 @@ function main() {
         document.body.appendChild(app);
     }
 
-    React.render(<div id="app">
-        <h1>React Typed Table Viewer</h1>
-        <div><button onClick={piper}>Fetch data</button></div>
-        <div><Tyyppi foo="hoh hoo" /></div>
-    </div>, app);
+    var reapp;
+
+    let data = {
+        arg: 0,
+        foo: "hoh hoo",
+        my_pipe: function () {
+            piping(
+                data,
+                (data) => {
+                    console.log("called back with ", data);
+                    reapp = React.render(
+                        <div id="app">
+                            <Main data={data} />
+                        </div>, app);
+                    console.log("re-rendering");
+                }
+            );
+        }
+    };
+
+    reapp = React.render(
+        <div id="app">
+            <Main data={data} />
+        </div>, app);
 }
 
 main();
