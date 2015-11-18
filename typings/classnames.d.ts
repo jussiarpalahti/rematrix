53f93d073f230e0ab794438b8483eb390aec15bf
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-11-18 17:37:33 +0200

    Basic module declaration for classnames package
    It works, but I don't know why

---
+++ typings/classnames.d.ts
@@ -0,0 +1,10 @@
+
+// NOTE: Check again module declarations, since this works for some reason...
+
+declare module "classnames" {
+    export module classnames {
+        export var classNames;
+    }
+}
+
+
