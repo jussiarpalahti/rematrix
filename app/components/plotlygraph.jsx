0d027a5b1a64bfe7a289fa07e59d7c6e34a70586
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-12-07 16:09:21 +0200

    Tested graph redraw with new data
    For the moment this is standalone, but it works with random
    data quite nicely.

--- app/components/plotlygraph.jsx
+++ app/components/plotlygraph.jsx
@@ -6,22 +6,6 @@ import React from 'react';
 
 var Plotly = require('../lib/plotly.js/dist/plotly.js');
 
-var data = {
-    x: [1, 2, 3, 4, 5, 6],
-    y: [10, 15, 12, 5, 7, 14],
-    name: 'eka',
-    type: 'bar'
-};
-
-var data2 = {
-    x: [1, 2, 3, 4, 5, 6],
-    y: [9, 15, 10, 5, 8, 14],
-    name: 'toka',
-    type: 'bar'
-};
-
-var all_data = [data, data2];
-
 var layout = {
     title: 'plotly.js with React',
     barmode: 'group',
@@ -32,11 +16,15 @@ let PViz = React.createClass({
 
     componentDidMount: function() {
         Plotly.newPlot(
-            this.refs.viz.getDOMNode(), all_data, layout
+            this.refs.viz.getDOMNode(), this.props.data, layout
         );
         this.refs.viz.getDOMNode().on('plotly_click', function(data){
             console.log("event with plotly", data);
         });
+    },
+
+    componentWillReceiveProps: function (nextProps) {
+        Plotly.redraw(this.refs.viz.getDOMNode());
     },
 
     shouldComponentUpdate: function() {
@@ -45,7 +33,7 @@ let PViz = React.createClass({
     },
 
     render: function () {
-        return <div ref="viz" id="plots">Jee</div>;
+        return <div ref="viz" id="plots"></div>;
     }
 });
 
@@ -58,9 +46,42 @@ function main() {
         app.setAttribute("id", "app");
         document.body.appendChild(app);
     }
-    React.render(<div id="app">
-        <PViz />
-    </div>, app);
+
+    var data = {
+        x: [1, 2, 3, 4, 5, 6],
+        y: [10, 15, 12, 5, 7, 14],
+        name: 'eka',
+        type: 'bar'
+    };
+
+    var data2 = {
+        x: [1, 2, 3, 4, 5, 6],
+        y: [9, 15, 10, 5, 8, 14],
+        name: 'toka',
+        type: 'bar'
+    };
+
+    var all_data = [data, data2];
+
+    function update() {
+        let min = 0, max = 100;
+        data.y = Array.apply(null, Array(data.y.length)).map((_, i) => {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        });
+        data2.y = Array.apply(null, Array(data.y.length)).map((_, i) => {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        });
+        rerender();
+    }
+
+    let rerender = () => {
+        React.render(<div id="app">
+            <button onClick={update}>Päivitä</button>
+            <PViz data={all_data} />
+        </div>, app);
+        console.log("re-rendering");
+    };
+    rerender();
 }
 
 main();
